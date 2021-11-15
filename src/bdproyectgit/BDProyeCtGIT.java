ff34376297da7483485ef77ba9bca4e00d4cc3e4
[Luis <[luheligo72@gmail.com]>
2021-11-15 10:29:21 -0500

    primer cambio

---
+++ src/bdproyectgit/BDProyeCtGIT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bdproyectgit;
+
+/**
+ *
+ * @author HP
+ */
+public class BDProyeCtGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String NOMBRE= "LUIS";
+        // TODO code application logic here
+    }
+    
+}
